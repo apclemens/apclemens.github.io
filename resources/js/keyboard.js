6ae54f39ce3a23ae2ae53f90691cdfa3d16c009b
Andrew Clemens <wikipedianerd@gmail.com>
2018-03-20 18:12:54 -0400

    Update keyboard.js

--- resources/js/keyboard.js
+++ resources/js/keyboard.js
@@ -6,16 +6,16 @@ window.onkeyup = function(e) {
             transition_down();
             break;
         case 80: // p
-            transition_to('/section_parts/projects', 0);
+            transition_to('/section_parts/projects', 0, '/projects', 'projects');
             break;
         case 87: // w
-            transition_to('/section_parts/websites', 1);
+            transition_to('/section_parts/websites', 1, '/websites', 'websites');
             break;
         case 84: // t
-            transition_to('/section_parts/themes', 2);
+            transition_to('/section_parts/themes', 2, '/themes', 'themes');
             break;
         case 67: // c
-            transition_to('/section_parts/contact', 3);
+            transition_to('/section_parts/contact', 3, '/contact', 'contact');
             break;
     }
 }
