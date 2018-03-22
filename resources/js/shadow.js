5f03d6dd002f78528fb516fef3fb561dd4b27aea
Andrew Clemens <wikipedianerd@gmail.com>
2018-03-21 21:45:56 -0400

    Update shadow.js

--- resources/js/shadow.js
+++ resources/js/shadow.js
@@ -1,7 +1,7 @@
 if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 window.onmousemove = function(e) {
-    var x = -(e.clientX - $(window).width()/2) / 25;
-    var y = -(e.clientY - $(window).height()/2) / 25;
+    var x = -(e.clientX - $(window).width()/2) / 10;
+    var y = -(e.clientY - $(window).height()/2) / 10;
     $('.shadow').css('box-shadow', ''+x+'px '+y+'px 50px var(--box-shadow-color)')
 }
 }
