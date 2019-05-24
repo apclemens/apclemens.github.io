3bfe96fa945a814e9302e4e4f6432aa8cd0ec946
Andrew Clemens <wikipedianerd@gmail.com>
2019-05-23 21:42:37 -0400

    Update keyboard.js

--- resources/js/keyboard.js
+++ resources/js/keyboard.js
@@ -5,14 +5,14 @@ window.onkeyup = function(e) {
         case 65: // a
             transition_down(true);
             break;
-        case 80: // p
-            transition_to('/section_parts/projects', 0, '/projects', 'andrew clemens - projects', true);
-            break;
         case 87: // w
-            transition_to('/section_parts/websites', 1, '/websites', 'andrew clemens - websites', true);
+            transition_to('/section_parts/websites', 0, '/websites', 'andrew clemens - websites', true);
             break;
-        case 84: // t
-            transition_to('/section_parts/web_apps', 2, '/web_apps', 'andrew clemens - web apps', true);
+        case 69: // e
+            transition_to('/section_parts/web_apps', 1, '/web_apps', 'andrew clemens - web apps', true);
+            break;
+        case 80: // p
+            transition_to('/section_parts/projects', 2, '/projects', 'andrew clemens - projects', true);
             break;
         case 67: // c
             transition_to('/section_parts/contact', 3, '/contact', 'andrew clemens - contact', true);
